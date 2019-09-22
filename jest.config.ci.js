69ee322dc8325d80b7da639c2ae9a4d27b0a5142
sota1235 <sota1235@gmail.com>
2019-09-22 19:19:49 +0900

    modify: add test command

---
+++ jest.config.ci.js
@@ -0,0 +1,8 @@
+const defaultConfig = require('./jest.config.js');
+
+module.exports = Object.assign({}, defaultConfig, {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}', '!**/node_modules/**', '!**/static/**', '!**/*.d.ts', '!jest.*'
+  ],
+});
---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: [
+    "<rootDir>/(pages|components)",
+  ],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: [
+    "ts",
+    "js",
+  ],
+};
