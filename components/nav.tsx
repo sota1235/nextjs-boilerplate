4ce8712e6e3b7f59ad4e46c34bc260a8a4674704
sota1235 <sota1235@gmail.com>
2019-09-22 19:06:22 +0900

    modify: change all files into typescript

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link'
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
+].map((link: { href: string, label: string, key?: string }) => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
-})
+});
 
 const Nav = () => (
   <nav>
@@ -51,6 +51,6 @@ const Nav = () => (
       }
     `}</style>
   </nav>
-)
+);
 
 export default Nav
