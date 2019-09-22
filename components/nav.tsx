d018d7e5e7656e999420806b3e2f3a03532d1e95
sota1235 <sota1235@gmail.com>
2019-09-22 19:15:15 +0900

    modify: install prettier and tslint

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import Link from 'next/link';
+import React from 'react';
 
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map((link: { href: string, label: string, key?: string }) => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
+  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
+].map((link: { href: string; label: string; key?: string }) => {
+  link.key = `nav-link-${link.href}-${link.label}`;
+  return link;
 });
 
 const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/'>
+        <Link href="/">
           <a>Home</a>
         </Link>
       </li>
@@ -53,4 +53,4 @@ const Nav = () => (
   </nav>
 );
 
-export default Nav
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import Head from 'next/head';
+import React from 'react';
+import Nav from '../components/nav';
 
 const Home = () => (
   <div>
@@ -10,24 +10,24 @@ const Home = () => (
 
     <Nav />
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
+    <div className="hero">
+      <h1 className="title">Welcome to Next.js!</h1>
+      <p className="description">
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
 
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
+      <div className="row">
+        <a href="https://nextjs.org/docs" className="card">
           <h3>Documentation &rarr;</h3>
           <p>Learn more about Next.js in the documentation.</p>
         </a>
-        <a href='https://nextjs.org/learn' className='card'>
+        <a href="https://nextjs.org/learn" className="card">
           <h3>Next.js Learn &rarr;</h3>
           <p>Learn about Next.js by following an interactive tutorial!</p>
         </a>
         <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
+          href="https://github.com/zeit/next.js/tree/master/examples"
+          className="card"
         >
           <h3>Examples &rarr;</h3>
           <p>Find other example boilerplates on the Next.js GitHub.</p>
@@ -82,6 +82,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
