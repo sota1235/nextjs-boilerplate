6332236d7f91148d37eaa6dc704d82120fe8e5c6
sota1235 <sota1235@gmail.com>
2019-09-25 21:13:29 +0900

    add: reducer for concert list

---
+++ src/reducers/concerts.ts
@@ -0,0 +1,18 @@
+import { ActionTypes } from '../actions/actionTypes';
+
+interface Concerts {
+  concerts: Concert[];
+}
+
+const initialState: Concerts = {
+  concerts: [],
+};
+
+const concerts = (state: Concerts = initialState, action: ActionTypes) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default concerts;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,5 @@
 import { combineReducers, Reducer } from 'redux';
+import concerts from './concerts';
 
-export const createRootReducer = (): Reducer => combineReducers({});
+export const createRootReducer = (): Reducer => combineReducers({ concerts });
 export type AppState = ReturnType<ReturnType<typeof createRootReducer>>;
