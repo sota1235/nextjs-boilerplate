6670667341a9d7d1e624273418e3114670c6061e
sota1235 <sota1235@gmail.com>
2019-09-25 19:21:24 +0900

    add: component for showing thumbnail

---
+++ src/components/ConcertThumbnail/ConcertThumbnail.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Image = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+`;
+
+interface Props {
+  title: string;
+  thumbnailURL: string;
+}
+
+const ConcertThumbnail: React.FC<Props> = ({ title, thumbnailURL }) => {
+  return <Image src={thumbnailURL} alt={title} />;
+};
+
+export { ConcertThumbnail };
