328e33b4e1d6714ac8b891df7b72de6b05afbb75
sota1235 <sota1235@gmail.com>
2020-03-06 21:53:27 +0900

    fix: add type annotation

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -2,6 +2,8 @@ import { addDecorator, configure } from '@storybook/react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/styles/theme';
 import React from 'react';
+import { StoryFn } from '@storybook/addons';
+import { StoryFnReactReturnType } from '@storybook/react/dist/client/preview/types';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /\.stories\.tsx$/);
@@ -9,7 +11,7 @@ function loadStories() {
   req.keys().forEach(req);
 }
 
-const BaseDecorator = storyFn => (
+const BaseDecorator = (storyFn: StoryFn<StoryFnReactReturnType>) => (
   <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
 );
 addDecorator(BaseDecorator);
