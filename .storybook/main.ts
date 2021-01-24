753e1b724be060b4b199c3d18b984db231f23241
sota1235 <sota1235@gmail.com>
2021-01-25 01:07:45 +0900

    modify: remove unnecessary workaround

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,23 +6,6 @@ const config: StorybookConfig = {
   typescript: {
     check: false,
   },
-  webpackFinal: async (config) => {
-    const { resolve } = config;
-    const alias = resolve?.alias || {};
-    alias['core-js/modules'] = '@storybook/core/node_modules/core-js/modules';
-    alias['core-js/features'] = '@storybook/core/node_modules/core-js/features';
-
-    if (resolve) {
-      resolve.alias = alias;
-      config.resolve = resolve;
-    } else {
-      config.resolve = {
-        alias,
-      };
-    }
-
-    return config;
-  },
   // @ts-ignore
   babel: async (options) => ({
     ...options,
