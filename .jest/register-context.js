9091be94d77c07ee6064ba9d7f7eb691552d57f6
sota1235 <sota1235@gmail.com>
2022-05-14 19:40:01 +0900

    modify: uninstall enzyme

--- .jest/register-context.js
+++
@@ -1,6 +0,0 @@
-const registerRequireContextHook = require('babel-plugin-require-context-hook/register');
-const Enzyme  = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
-
-Enzyme.configure({ adapter: new Adapter() });
-registerRequireContextHook();
