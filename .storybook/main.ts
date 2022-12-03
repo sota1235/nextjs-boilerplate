b767a7bff7f6dab55c900b857b00ec376200f21e
sota1235 <sota1235@gmail.com>
2022-12-03 17:51:16 +0900

    modify: upgrade to v13

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,24 +1,20 @@
-import { StorybookConfig } from '@storybook/react/types/index';
-
+import { StorybookConfig } from '@storybook/types';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   typescript: {
-    check: false,
+    check: false
   },
-  // @ts-ignore
-  babel: async (options) => ({
+  framework: '@storybook/react-vite',
+  babel: async options => ({
     ...options,
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          useBuiltIns: 'usage',
-          corejs: 3,
-        },
-      ],
-    ],
+    presets: [['@babel/preset-env', {
+      useBuiltIns: 'usage',
+      corejs: 3
+    }]]
   }),
+  docs: {
+    docsPage: 'automatic'
+  }
 };
-
 export default config;
